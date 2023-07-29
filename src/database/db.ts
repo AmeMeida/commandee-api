53c1a97695e82ca147f7947486847b00e27ed9db
AmeMeida <ame.pistache@gmail.com>
2023-07-29 00:29:17 -0300

    formatando

--- src/database/db.ts
+++ src/database/db.ts
@@ -5,5 +5,3 @@ import * as schema from "./schema";
 const pool = mysql.createPool(import.meta.env.DATABASE_URL);
 
 export default drizzle(pool, { schema });
-
-
--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -3,9 +3,12 @@ import { FastifyInstance } from "..";
 import db from "../database/db";
 import { order } from "../database/schema";
 
-export default async function(fastify: FastifyInstance) {
+export default async function (fastify: FastifyInstance) {
   fastify.get("/hello", async (_request, reply) => {
-    const orders = await db.select().from(order).where(eq(order.status, "done"));
+    const orders = await db
+      .select()
+      .from(order)
+      .where(eq(order.status, "done"));
     return reply.send(orders);
   });
 }
